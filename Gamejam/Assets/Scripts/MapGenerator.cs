af792b6bd2b3523eed4f5b92ae170dce93861dbc
FlorentDesbiens <f.desbiens5910@gmail.com>
2022-12-03 14:00:30 -0500

    Generate Ore

--- Gamejam/Assets/Scripts/MapGenerator.cs
+++ Gamejam/Assets/Scripts/MapGenerator.cs
@@ -10,44 +10,42 @@ public class MapGenerator : MonoBehaviour
     public int minCol = 10;
     public int maxCol = 20;
     public int chanceOfGround = 4;
+    public int oreAmount = 10;
 
-    private int[,] _tiles;
+    private enum TILES { GROUND, WALL, ORE}
+    private TILES[,] _tiles;
+    private Vector2Int[] _directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
 
     [SerializeField] private Grid _grid;
     [SerializeField] private GameObject _groundTile;
     [SerializeField] private GameObject _wallTile;
+    [SerializeField] private GameObject _oreTile;
 
     // Start is called before the first frame update
     void Start()
     {
         _grid.RowCount = (uint)Random.Range(minRow, maxRow);
         _grid.ColumnCount = (uint)Random.Range(minCol, maxCol);
-        _tiles = new int[_grid.ColumnCount, _grid.RowCount];
+        _tiles = new TILES[_grid.ColumnCount, _grid.RowCount];
 
 
         for (int x = 0; x < _grid.ColumnCount; x++) {
             for (int y = 0; y < _grid.RowCount; y++) {
-                _tiles[x, y] = 0;
+                _tiles[x, y] = TILES.WALL;
             }
         }
 
         int t_spawnPosX = Random.Range(1, (int)_grid.ColumnCount - 1);
         int t_spawnPosY = Random.Range(1, (int)_grid.RowCount - 1);
-        _tiles[t_spawnPosX, t_spawnPosY] = 1;
+        _tiles[t_spawnPosX, t_spawnPosY] = TILES.GROUND;
 
-        GenerateAround(t_spawnPosX, t_spawnPosY);
-
+        GenerateGroundAround(t_spawnPosX, t_spawnPosY, true);
+        GenerateOres();
         PlaceTiles();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void PlaceTile(GameObject a_tile, int x, int y) {
-        Vector3 t_worldPos = _grid.GridToWorld(new Vector2Int(x, y));
+    void PlaceTile(GameObject a_tile, int a_x, int a_y) {
+        Vector3 t_worldPos = _grid.GridToWorld(new Vector2Int(a_x, a_y));
 
         GameObject t_newTile = Instantiate(a_tile, _grid.transform);
         t_newTile.transform.position = t_worldPos;
@@ -56,49 +54,81 @@ public class MapGenerator : MonoBehaviour
         t_newTile.transform.localScale = new Vector3(t_newScale, t_newScale, t_newScale);
 
         Tile t_tile = a_tile.GetComponent<Tile>();
-        t_tile.x = (uint)x;
-        t_tile.y = (uint)y;
+        t_tile.x = (uint)a_x;
+        t_tile.y = (uint)a_y;
     }
 
-    void GenerateAround(int x, int y) {
-        Vector2Int[] t_directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+    void GenerateGroundAround(int a_x, int a_y, bool a_isFirstGen = false) {
         List<Vector2Int> t_validTiles = new List<Vector2Int>();
         bool t_isGroundPlace = false;
-        foreach (Vector2Int t_direction in t_directions) {
-            int t_x = x + t_direction.x;
-            int t_y = y + t_direction.y;
+        foreach (Vector2Int t_direction in _directions) {
+            int t_x = a_x + t_direction.x;
+            int t_y = a_y + t_direction.y;
 
-            if (_tiles[t_x, t_y] == 0 && t_x != 0 && t_x != _grid.ColumnCount - 1 && t_y != 0 && t_y != _grid.RowCount - 1) {
-                if (((t_direction == Vector2Int.up || t_direction == Vector2Int.down) && _tiles[t_x - 1, t_y] == 0 && _tiles[t_x + 1, t_y] == 0) || ((t_direction == Vector2Int.left || t_direction == Vector2Int.right) && _tiles[t_x, t_y - 1] == 0 && _tiles[t_x, t_y + 1] == 0))
+            if (_tiles[t_x, t_y] == TILES.WALL && t_x != 0 && t_x != _grid.ColumnCount - 1 && t_y != 0 && t_y != _grid.RowCount - 1) {
+                if (((t_direction == Vector2Int.up || t_direction == Vector2Int.down) && _tiles[t_x - 1, t_y] == TILES.WALL && _tiles[t_x + 1, t_y] == TILES.WALL) || ((t_direction == Vector2Int.left || t_direction == Vector2Int.right) && _tiles[t_x, t_y - 1] == TILES.WALL && _tiles[t_x, t_y + 1] == TILES.WALL))
                     t_validTiles.Add(new Vector2Int(t_x, t_y));
             }
         }
 
         foreach (Vector2Int tile in t_validTiles) {
             if (Random.Range(1, chanceOfGround) == 1) {
-                _tiles[tile.x, tile.y] = 1;
-                GenerateAround(tile.x, tile.y);
+                _tiles[tile.x, tile.y] = TILES.GROUND;
+                GenerateGroundAround(tile.x, tile.y);
                 t_isGroundPlace = true;
             }
         }
 
         if (!t_isGroundPlace && t_validTiles.Count != 0) {
-            Vector2Int tile = t_validTiles[Random.Range(0, t_validTiles.Count - 1)];
-            _tiles[tile.x, tile.y] = 1;
-            GenerateAround(tile.x, tile.y);
+            Vector2Int t_tile = t_validTiles[Random.Range(0, t_validTiles.Count - 1)];
+            _tiles[t_tile.x, t_tile.y] = TILES.GROUND;
+            GenerateGroundAround(t_tile.x, t_tile.y);
+            if (a_isFirstGen) { 
+                t_validTiles.Remove(t_tile);
+                t_tile = t_validTiles[Random.Range(0, t_validTiles.Count - 1)];
+                _tiles[t_tile.x, t_tile.y] = TILES.GROUND;
+                GenerateGroundAround(t_tile.x, t_tile.y);
+            }
+        }
+    }
+
+    void GenerateOres() {
+        List<Vector2Int> t_validTiles = new List<Vector2Int>();
+        for (int x = 0; x < _grid.ColumnCount; x++) {
+            for (int y = 0; y < _grid.RowCount; y++) {
+                if (_tiles[x, y] == TILES.WALL) {
+                    foreach (Vector2Int t_direction in _directions) {
+                        int t_x = x + t_direction.x;
+                        int t_y = y + t_direction.y;
+                        if (t_x >= 0 && t_x < _grid.ColumnCount && t_y >= 0 && t_y < _grid.RowCount) {
+                            if (_tiles[t_x, t_y] == TILES.GROUND) {
+                                t_validTiles.Add(new Vector2Int(x, y));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < oreAmount; i++) {
+            Vector2Int t_tile = t_validTiles[Random.Range(0, t_validTiles.Count - 1)];
+            _tiles[t_tile.x, t_tile.y] = TILES.ORE;
+            t_validTiles.Remove(t_tile);
         }
     }
 
     void PlaceTiles() {
         for (int x = 0; x < _grid.ColumnCount; x++) {
             for (int y = 0; y < _grid.RowCount; y++) {
-                Debug.Log(x + ", " + y);
                 switch (_tiles[x, y]) {
-                    case 0:
+                    case TILES.WALL:
                         PlaceTile(_wallTile, x, y);
                         break;
-                    case 1:
+                    case TILES.GROUND:
                         PlaceTile(_groundTile, x, y);
+                        break;
+                    case TILES.ORE:
+                        PlaceTile(_oreTile, x, y);
                         break;
                     default:
                         break;
