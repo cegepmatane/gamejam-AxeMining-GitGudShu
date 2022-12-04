fa5c5a3fbb63e4d340d902931abe3c95112e0f6e
FlorentDesbiens <f.desbiens5910@gmail.com>
2022-12-04 16:53:38 -0500

    Camera

--- Gamejam/Assets/Scripts/CameraScript.cs
+++ Gamejam/Assets/Scripts/CameraScript.cs
@@ -12,22 +12,38 @@ public class CameraScript : MonoBehaviour
     private float posX;
     private float posY;
 
+    private float _gridWidth;
+    private float _gridHeight;
+    private float _cameraHalfWidth;
+    private float _cameraHalfHeight;
+
+
     [SerializeField] private Grid grid;
     [SerializeField] private Transform target;
 
     void Update()
     {
         if (target == null) return;
+
+        _gridHeight = grid.RowCount * grid.CellSize;
+        _gridWidth = grid.ColumnCount * grid.CellSize;
+        _cameraHalfHeight = Camera.main.orthographicSize;
+        _cameraHalfWidth = Camera.main.aspect * Camera.main.orthographicSize;
+
         
         minX = Mathf.Min(minX, grid.transform.position.x);
-        maxX = Mathf.Max(maxX, grid.transform.position.x + grid.ColumnCount * grid.CellSize);
+        maxX = Mathf.Max(maxX, grid.transform.position.x + _gridWidth);
         minY = Mathf.Min(minY, grid.transform.position.y);
-        maxY = Mathf.Max(maxY, grid.transform.position.y + grid.RowCount * grid.CellSize);
+        maxY = Mathf.Max(maxY, grid.transform.position.y + _gridHeight);
 
-        posY = Mathf.Max(Mathf.Min(target.position.y, maxY - Camera.main.orthographicSize), minY + Camera.main.orthographicSize);
-        posX = Mathf.Max(Mathf.Min(target.position.x, maxX - Camera.main.aspect * Camera.main.orthographicSize), minX + Camera.main.aspect * Camera.main.orthographicSize);
+        posY = Mathf.Max(Mathf.Min(target.position.y, maxY - _cameraHalfHeight), minY + _cameraHalfHeight);
+        posX = Mathf.Max(Mathf.Min(target.position.x, maxX - _cameraHalfWidth), minX + _cameraHalfWidth);
+
+        if (_gridWidth <= _cameraHalfWidth * 2) posX = grid.transform.position.x + _gridWidth / 2;
+        if (_gridHeight <= _cameraHalfHeight * 2) posY = grid.transform.position.y + _gridHeight / 2;
+
+        print(_gridHeight + " " + _cameraHalfHeight);
+
         transform.position = new Vector3(posX, posY, transform.position.z);
-
-        print(maxX);
     }
 }
