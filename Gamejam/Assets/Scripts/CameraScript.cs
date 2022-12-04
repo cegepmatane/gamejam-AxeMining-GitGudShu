fb54195e2ce44f59af718df71f580e79200fcf92
FlorentDesbiens <f.desbiens5910@gmail.com>
2022-12-04 16:23:42 -0500

    Camera

--- Gamejam/Assets/Scripts/CameraScript.cs
+++ Gamejam/Assets/Scripts/CameraScript.cs
@@ -15,24 +15,19 @@ public class CameraScript : MonoBehaviour
     [SerializeField] private Grid grid;
     [SerializeField] private Transform target;
 
-    void Start()
-    {
-
-    }
-
     void Update()
     {
         if (target == null) return;
         
         minX = Mathf.Min(minX, grid.transform.position.x);
-        maxX = Mathf.Max(maxX, grid.transform.position.x + (grid.ColumnCount - 1) * grid.CellSize);
-        minX = Mathf.Min(minY, grid.transform.position.y);
-        maxX = Mathf.Max(maxY, grid.transform.position.y + (grid.RowCount - 1) * grid.CellSize);
+        maxX = Mathf.Max(maxX, grid.transform.position.x + grid.ColumnCount * grid.CellSize);
+        minY = Mathf.Min(minY, grid.transform.position.y);
+        maxY = Mathf.Max(maxY, grid.transform.position.y + grid.RowCount * grid.CellSize);
 
-        posY = Mathf.Max(target.position.y, minY + Camera.main.orthographicSize);
+        posY = Mathf.Max(Mathf.Min(target.position.y, maxY - Camera.main.orthographicSize), minY + Camera.main.orthographicSize);
         posX = Mathf.Max(Mathf.Min(target.position.x, maxX - Camera.main.aspect * Camera.main.orthographicSize), minX + Camera.main.aspect * Camera.main.orthographicSize);
         transform.position = new Vector3(posX, posY, transform.position.z);
 
-        print(grid.transform.position.x + " " + transform.position.x + " " + Camera.main.aspect * Camera.main.orthographicSize);
+        print(maxX);
     }
 }
